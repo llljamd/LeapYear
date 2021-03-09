bffce5b4252d3141e46264e3e29202fef65203a7
Alex Nguyen <nguyeal2@oregonstate.edu>
2021-03-08 23:00:33 -0800

    added test2

--- leap_test.py
+++ leap_test.py
@@ -5,5 +5,8 @@ class test(unittest.TestCase):
 	def test1(self):
 		self.assertEqual(leap_code.leap(2004), "2004 is a leap year");
 
+	def test2(self):
+		self.assertEqual(leap_code.leap(2100), "2100 is not a leap year");
+
 if __name__ == '__main__':
 	unittest.main()

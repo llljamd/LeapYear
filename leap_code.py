89b9596be7eccb24b1979017d9e5c7b23d35de51
Alex Nguyen <nguyeal2@oregonstate.edu>
2021-03-08 23:03:18 -0800

    code for test3

--- leap_code.py
+++ leap_code.py
@@ -1,7 +1,11 @@
 def leap(x):
 	if x % 4 == 0:
 		if x % 100 == 0:
-			return str(x) + " is not a leap year";
+			if x % 400 == 0:
+				return str(x) + " is a leap year";
+
+			else:
+				return str(x) + " is not a leap year";
 		
 		else:	
 			return str(x) + " is a leap year";
